030fe4592c486109e1edad3962586f286b53859f
lin <529029157@qq.com>
2020-08-08 22:08:05 +0800

    master 添加 hellocontroller

---
+++ src/main/java/com/lin/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.lin.controller;
+
+/**
+ * @author: Mr.Lin
+ * @create: 2020-08-08 22:07:11
+ **/
+public class HelloController {
+    public void say(String str){
+        System.out.println(str);
+        System.out.println(str);
+    }
+}
