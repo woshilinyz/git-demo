7e8ad8009965798f52f902da141a2a04a7cffd5a
lin <529029157@qq.com>
2020-08-08 22:09:43 +0800

    branch 添加新功能

---
+++ src/main/java/com/lin/controller/HiController.java
@@ -0,0 +1,11 @@
+package com.lin.controller;
+
+/**
+ * @author: Mr.Lin
+ * @create: 2020-08-08 22:08:54
+ **/
+public class HiController {
+    public void say(String str){
+        System.out.println(str);
+    }
+}
