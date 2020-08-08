afef5558715b8c9d9883eff5cd74ce2d09f66c87
lin <529029157@qq.com>
2020-08-08 22:21:23 +0800

    Merge branch 'master' into feature/测试
    # Conflicts:
    #	src/main/java/com/lin/utils/MathUtil.java

--- src/main/java/com/lin/utils/MathUtil.java
+++ src/main/java/com/lin/utils/MathUtil.java
@@ -5,11 +5,10 @@ package com.lin.utils;
  * @create: 2020-08-08 22:14:47
  **/
 public class MathUtil {
-    public double add(double v1,double v2){
-        return v1+v2;
- * @create: 2020-08-08 22:12:10
- **/
-public class MathUtil {
+    public double add(double v1,double v2) {
+        return v1 + v2;
+
+    }
     public int add(int a,int b){
         return a+b;
     }
